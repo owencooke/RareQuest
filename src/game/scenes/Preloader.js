1192a5b9ba51b2ca3d37f98b2459a6b2d439a0af
Owen Cooke <ocooke@ualberta.ca>
2024-03-23 02:04:14 -0600

    fix progress bar and page title

--- src/game/scenes/Preloader.js
+++ src/game/scenes/Preloader.js
@@ -6,16 +6,27 @@ export class Preloader extends Scene {
     }
 
     init() {
-        //  A simple progress bar. This is the outline of the bar.
-        this.add.rectangle(512, 384, 468, 32).setStrokeStyle(1, 0xffffff);
+        const centerX = this.cameras.main.centerX;
+        const centerY = this.cameras.main.centerY;
+        const progressBarWidth = this.scale.width * 0.5;
+
+        // A simple progress bar. This is the outline of the bar.
+        this.add
+            .rectangle(centerX, centerY, progressBarWidth, 32)
+            .setStrokeStyle(1, 0xffffff);
 
         //  This is the progress bar itself. It will increase in size from the left based on the % of progress.
-        const bar = this.add.rectangle(512 - 230, 384, 4, 28, 0xffffff);
+        const bar = this.add.rectangle(
+            centerX - progressBarWidth / 2,
+            centerY,
+            4,
+            28,
+            0xffffff
+        );
 
-        //  Use the 'progress' event emitted by the LoaderPlugin to update the loading bar
+        // Update the loading bar
         this.load.on("progress", (progress) => {
-            //  Update the progress bar (our bar is 464px wide, so 100% = 464px)
-            bar.width = 4 + 460 * progress;
+            bar.width = progressBarWidth * progress;
         });
     }
 
