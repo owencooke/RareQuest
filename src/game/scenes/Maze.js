47c412d9f3121d6a009a320edd6969071da47d3f
Pratham Sitoula <psitoula2003@gmail.com>
2024-03-23 11:32:36 -0600

    Added assets

--- src/game/scenes/Maze.js
+++ src/game/scenes/Maze.js
@@ -5,23 +5,19 @@ import { Scene } from 'phaser';
 export class Maze extends Scene {
   constructor() {
     super('Maze');
+    this.player;
+    this.cursors;
+    this.currentDirection = 'down';
   }
 
-  create() {
-    this.cameras.main.setBackgroundColor(0x000000);
-
-    this.add.image(512, 384, 'background').setAlpha(0.5);
-
-    this.add.text(512, 384, 'Maze', {
-      fontFamily: 'Arial Black', fontSize: 64, color: '#ffffff',
-      stroke: '#000000', strokeThickness: 8,
-      align: 'center'
-    }).setOrigin(0.5).setDepth(100);
-
-    EventBus.emit('current-scene-ready', this);
+  preload() {
+    this.load.tilemapTiledJSON('maze', './assets/maze.json');
+    this.load.image('maze-tiles', './assets/maze-tiles.png');
+    this.load.spritesheet('player', './assets/player.png', {
+      frameWidth: 16,
+      frameHeight: 16,
+    });
   }
 
-  changeScene() {
-    this.scene.start('MainMenu');
-  }
+
 }
