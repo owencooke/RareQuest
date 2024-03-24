a3e9f552f95347e593c233aca2744d486da4cd0f
Bhagya <bhagya02.p@gmail.com>
2024-03-24 09:44:42 -0600

    export scences

--- src/game/scenes/index.js
+++ src/game/scenes/index.js
@@ -3,13 +3,10 @@ import { City } from "./City";
 import { Hospital } from "./hospitalRoom"
 import { MainMenu } from "./MainMenu";
 import { Preloader } from "./Preloader";
-import { ChooseCharacter } from "./ChooseCharacter";
 import { TileJump } from "./TileJump";
 import { GameOverTileJump } from "./GameOverTileJump";
 import { Maze } from "./Maze";
 
 // IMPORTANT: you must import and add your new scene to this array for it to be renderable!!
-export const scenes = [Boot, Preloader, MainMenu, City, Hospital    TileJump,
-    GameOverTileJump,
-];
+export const scenes = [Boot, Preloader, MainMenu, City, Hospital, TileJump, GameOverTileJump];
 
