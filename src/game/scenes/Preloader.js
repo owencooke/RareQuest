0e4992ed5c70c887dd23863628f2ae38cf60a876
roger-chau <54104821+roger-chau@users.noreply.github.com>
2024-03-24 20:34:25 -0600

    fixed loading into pong

--- src/game/scenes/Preloader.js
+++ src/game/scenes/Preloader.js
@@ -125,7 +125,7 @@ export class Preloader extends Scene {
         });
 
         //  Move to the MainMenu. You could also swap this for a Scene Transition, such as a camera fade.
-        this.scene.start("Pong");
+        this.scene.start("MainMenu");
     }
 
     createPlayerRunAnimations() {
