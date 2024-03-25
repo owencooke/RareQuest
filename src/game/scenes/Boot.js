9bf4b0ec63039a9c03952fb55190a28fe40b9c43
Sami Jagirdar <jagirdar@ualberta.ca>
2024-03-25 03:10:00 -0600

    added relevant local storage variables

--- src/game/scenes/Boot.js
+++ src/game/scenes/Boot.js
@@ -13,6 +13,56 @@ export class Boot extends Scene {
 
     create() {
         this.scene.start("Preloader");
+
+        //Total Mini games played local storage variable
+        if (!localStorage.getItem('miniGamesCompleted')) {
+            localStorage.setItem('miniGamesCompleted', 0);
+        }
+        //Maze games played
+        if (!localStorage.getItem('mazeGamesCompleted')) {
+            localStorage.setItem('mazeGamesCompleted', 0);
+        }
+        //Pong games played
+        if (!localStorage.getItem('pongGamesCompleted')) {
+            localStorage.setItem('pongGamesCompleted', 0);
+        }
+        //Spy games played
+        if (!localStorage.getItem('SpyGamesCompleted')) {
+            localStorage.setItem('SpyGamesCompleted', 0);
+        }
+        //Jump games played
+        if (!localStorage.getItem('jumpGamesCompleted')) {
+            localStorage.setItem('jumpGamesCompleted', 0);
+        }
+        // Catcher games played
+        if (!localStorage.getItem('catcherGamesCompleted')) {
+            localStorage.setItem('catcherGamesCompleted', 0);
+        }
+        //Total doctor interactions
+        if (!localStorage.getItem('totalDoctorInteractions')) {
+            localStorage.setItem('totalDoctorInteractions', 0);
+        }
+        //Eye doctor interactions
+        if (!localStorage.getItem('eyeDoctorInteractions')) {
+            localStorage.setItem('numDoctorInteractions', 0);
+        }
+        //derma doctor interactions
+        if (!localStorage.getItem('dermaDoctorInteractions')) {
+            localStorage.setItem('dermaDoctorInteractions', 0);
+        }
+        //neuro doctor interactions
+        if (!localStorage.getItem('neuroDoctorInteractions')) {
+            localStorage.setItem('neuroDoctorInteractions', 0);
+        }
+        //pulm doctor interactions
+        if (!localStorage.getItem('pulDoctorInteractions')) {
+            localStorage.setItem('pulDoctorInteractions', 0);
+        }
+        //pedia doctor interactions
+        if (!localStorage.getItem('pediaDoctorInteractions')) {
+            localStorage.setItem('pediaDoctorInteractions', 0);
+        }
+
     }
 }
 
