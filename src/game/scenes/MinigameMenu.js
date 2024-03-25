a79d764fab5872a71eb8af873c4c31d3395595e5
Owen Cooke <ocooke@ualberta.ca>
2024-03-25 01:48:12 -0600

    fix minigame menu styling

--- src/game/scenes/MinigameMenu.js
+++ src/game/scenes/MinigameMenu.js
@@ -3,10 +3,15 @@ import { Scene } from "phaser";
 const howToPlay = {
     TileJump:
         "Jump up to the next rising layer without falling! Use the left and right arrow keys to move horizontally, and press the spacebar or the up arrow key to jump.",
-
     Pong: "Keep the ball up using your head and to prevent the ball from hitting the ground. Move left and right using the arrow keys.",
-
     Maze: "Navigate through the maze to reach the zebra-striped goal. Use the arrow keys to move up, down, left, and right.",
+};
+
+const textStyle = {
+    fontSize: "24px",
+    fill: "black",
+    align: "center",
+    fontFamily: "'Press Start 2P'",
 };
 
 export class MinigameMenu extends Scene {
@@ -19,47 +24,55 @@ export class MinigameMenu extends Scene {
     }
 
     create() {
-        let background = this.add.graphics();
-        background.fillStyle(0x000000, 1);
-        background.fillRect(
-            0,
-            0,
-            this.game.config.width,
-            this.game.config.height
+        // Background
+
+        const background = this.add.image(
+            this.cameras.main.width / 2,
+            this.cameras.main.height / 2,
+            "bg"
+        );
+        background.setDisplaySize(
+            this.cameras.main.width,
+            this.cameras.main.height
         );
 
         // Title text
         this.add
             .text(
                 this.game.config.width / 2,
-                this.game.config.height / 3,
-                this.title,
-                { fontSize: "48px", fill: "#ffffff", align: "center" }
+                this.game.config.height / 4,
+                this.minigame,
+                { ...textStyle, fontSize: "48px" }
             )
             .setOrigin(0.5);
 
-        // Instructions
+        // Instructions text
         this.add
             .text(
                 this.game.config.width / 2,
                 this.game.config.height / 2,
-                this.instructions,
-                { fontSize: "24px", fill: "#ffffff", align: "center" }
-            )
-            .setOrigin(0.5)
-            .setText(howToPlay[this.minigame]);
-
-        this.startText = this.add
-            .text(
-                this.game.config.width / 2,
-                (this.game.config.height * 2) / 3,
-                "Click to Start",
-                { fontSize: "24px", fill: "#ffffff", align: "center" }
+                howToPlay[this.minigame],
+                {
+                    ...textStyle,
+                    wordWrap: { width: this.game.config.width - 100 },
+                }
             )
             .setOrigin(0.5);
 
-        this.startText.setInteractive();
-        this.startText.on("pointerdown", this.startGame, this);
+        // Click to start text
+        let startText = this.add
+            .text(
+                this.game.config.width / 2,
+                (this.game.config.height * 3) / 4,
+                "Click Anywhere or Press Spacebar to Start!",
+                textStyle
+            )
+            .setOrigin(0.5);
+
+        // Make text interactive
+        startText.setInteractive();
+        startText.on("pointerdown", this.startGame, this);
+        this.input.keyboard.on("keydown-SPACE", this.startGame, this);
     }
 
     startGame() {
